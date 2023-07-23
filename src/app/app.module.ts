eb53332a75ec7f9c37cbf82a9dff0a5e304e11b5
Mauricio Fajardo <jmauriciofajardo@gmail.com>
2023-07-23 12:11:15 -0500

    add not found page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CardComponent } from './components/card/card.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
