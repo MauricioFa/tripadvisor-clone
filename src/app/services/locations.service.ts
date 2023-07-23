a77d6051dc726b7f8fd83c1a56883236390da37e
Mauricio Fajardo <jmauriciofajardo@gmail.com>
2023-07-23 10:04:53 -0500

    update environment path

--- src/app/services/locations.service.ts
+++ src/app/services/locations.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,6 @@ export class LocationsService {
   
   getLocations(search: string){
     let apiUrl = encodeURI(`${environment.API_URL}/api/search?searchQuery=${search}`)
-    console.log({ apiUrl })
     return this.http.get<any>(apiUrl)
   }
 }
